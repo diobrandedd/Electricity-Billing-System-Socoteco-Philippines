b69f71c47ab26989ea6cb5c14ec9d0fc3935ef3e
diobrandedd <diobranded69@gmail.com>
2025-09-07 23:12:19 +0800

    users update
    asfadfa

---
+++ users/userindex.php
@@ -0,0 +1,19 @@
+<doctype html>
+    <head>
+        <title>Socoteco II</title>
+    </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <body>
+    <nav class="navbar navbar-light" style="background-color: #F7B32B">
+  <div class="container-fluid ">
+    <a class="navbar-brand" href="#">
+      <img src="../img/socotecoL.png" alt="" width="300" height="60" class="d-inline-block align-text-top">
+    </a>
+  </div>
+</nav>
+        <h1>User Index</h1>
+    </body>
+    
+</doctype>
